431418d181444845a9f3e091dbce4c87b57af09a
birdierabi <vnefremova8@gmail.com>
2020-10-14 17:25:10 +0300

    feat: add morse-decoder solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,18 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let decodeValue = [];
+  for (let i = 0; i < expr.length; i += 10) {
+    decodeValue.push(expr.slice(i, i + 10).replace(/10/g, '.').replace(/11/g, '-').replace(/\*{10}/g, ' ').replace(/0/g, ''));
+  };
+
+  for (let i = 0; i < decodeValue.length; i++) {
+    decodeValue[i] = MORSE_TABLE[decodeValue[i]];
+    if (decodeValue[i] === undefined) {
+      decodeValue[i] = ' ';
+    };
+  };
+  return decodeValue.join('');
 }
 
 module.exports = {
